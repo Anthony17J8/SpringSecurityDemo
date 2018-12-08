a281f164e9e03d40bb8b6d864283f5359956c911
Kiselev <j8kiselev@gmail.com>
2018-12-08 16:50:18 +0300

    Authentication and authorization with JDBC Data Base

--- src/main/java/ru/icoltd/springsecurity/demo/config/AppConfig.java
+++ src/main/java/ru/icoltd/springsecurity/demo/config/AppConfig.java
@@ -1,20 +1,37 @@
 package ru.icoltd.springsecurity.demo.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Objects;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "ru.icoltd.springsecurity.demo")
+@PropertySource("classpath:properties/persistence-mysql.properties")
 public class AppConfig {
+
+    // set up variable to hold the properties
+    @Autowired
+    private Environment env;
+
+    // set up a logger for diagnostics
+    private Logger logger = Logger.getLogger(getClass().getName());
 
     // define a bean for ViewResolver
     @Bean
-    public ViewResolver viewResolver(){
+    public ViewResolver viewResolver() {
 
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 
@@ -22,5 +39,42 @@ public class AppConfig {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    // define a bean for security DataSource
+    @Bean
+    public DataSource securityDataSource() {
+
+        // create connection pool
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        try {
+            // set the jdbc drive class
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException exc) {
+            throw new RuntimeException(exc);
+        }
+        // log the connection props
+        logger.info(">>> jdbc.url = " + env.getProperty("jdbc.url"));
+        logger.info(">>> jdbc.user = " + env.getProperty("jdbc.user"));
+
+        // set database connection props
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        // set connection pool props
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    // create a helper method to read env property and convert to int
+    private int getIntProperty(String propName) {
+        String propVal = env.getProperty(propName);
+        return Integer.parseInt(Objects.requireNonNull(propVal));
     }
 }
